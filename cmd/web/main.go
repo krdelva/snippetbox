f2412ecff18715e94541a421c22d613fe8b0d125
krdelva <kevin.r.delva@gmail.com>
2025-05-08 13:05:14 -0400

    add structured logging with log/slog and output to os.Stdout

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -2,16 +2,18 @@ package main
 
 import (
 	"flag"
-	"log"
+	"log/slog"
 	"net/http"
+	"os"
 	"strings"
 )
 
 func main() {
 
 	addr := flag.String("addr", ":4000", "HTTP network address")
-
 	flag.Parse()
+
+	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
 
 	mux := http.NewServeMux()
 
@@ -24,10 +26,11 @@ func main() {
 	mux.HandleFunc("GET /snippet/create", snippetCreate)
 	mux.HandleFunc("POST /snippet/create", snippetCreatePost)
 
-	log.Printf("starting server on %s", *addr)
+	logger.Info("starting server", "addr", *addr)
 
 	err := http.ListenAndServe(*addr, mux)
-	log.Fatal(err)
+	logger.Error(err.Error())
+	os.Exit(1)
 }
 
 func neuter(next http.Handler) http.Handler {
